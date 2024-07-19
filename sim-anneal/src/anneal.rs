2b38764b7a54d569b1c89cf335583b010d6ca216
Archim Jhunjhunwala <archim.jhunjhunwala@gmail.com>
2024-07-19 17:47:39 -0400

    sim anneal, probly doesn't work

---
+++ sim-anneal/src/anneal.rs
@@ -0,0 +1,227 @@
+use itertools::Itertools;
+use pyo3::PyResult;
+use std::collections::BTreeMap;
+use std::fmt::Debug;
+use std::num::Wrapping;
+
+const MAX: f64 = std::u64::MAX as f64;
+const MAX_POP_DIFF: f64 = 0.05;
+
+#[inline]
+fn rotl(x: u64, k: i32) -> u64 {
+    (x << k) | (x >> (64 - k))
+}
+
+#[derive(Debug)]
+pub struct UniformDist([u64; 2]);
+impl UniformDist {
+    fn next(&mut self) -> u64 {
+        let s0 = Wrapping(self.0[0]);
+        let mut s1 = Wrapping(self.0[1]);
+        let result = s0 + s1;
+        s1 ^= s0;
+        self.0[0] = (Wrapping(rotl(s0.0, 55)) ^ s1 ^ (s1 << 14)).0;
+        self.0[1] = rotl(s1.0, 36);
+        result.0
+    }
+
+    fn new(seed: [u64; 2]) -> UniformDist {
+        Self([seed[0], seed[1]])
+    }
+}
+
+pub struct Annealer<F: Fn(&[Vec<bool>]) -> f64, G: Fn(f64) -> f64> {
+    objective: F,
+    temperature: G,
+    precinct_in: Vec<usize>,
+    adj: Vec<Vec<usize>>,
+    cur_state: (f64, Vec<usize>),
+    best: (f64, Vec<usize>),
+    num_districts: usize,
+    population: Vec<usize>,
+    border_nodes: BTreeMap<usize, usize>,
+}
+
+impl<F: Fn(&[Vec<bool>]) -> f64, G: Fn(f64) -> f64> Debug for Annealer<F, G> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("precinct_in: {:?}", self.precinct_in))?;
+        f.write_fmt(format_args!("adj: {:?}", self.adj))?;
+        f.write_fmt(format_args!("cur_state: {:?}", self.cur_state))?;
+        f.write_fmt(format_args!("best_state: {:?}", self.best))?;
+        f.write_fmt(format_args!("num_districts: {}", self.num_districts))?;
+        f.write_fmt(format_args!("population: {:?}", self.population))?;
+        f.write_fmt(format_args!("border_nodes: {:?}", self.border_nodes))?;
+        Ok(())
+    }
+}
+
+impl<F: Fn(&[Vec<bool>]) -> f64, G: Fn(f64) -> f64> Annealer<F, G> {
+    pub fn new(
+        precinct_in: Vec<usize>,
+        adj: Vec<Vec<usize>>,
+        objective: F,
+        temperature: G,
+        num_districts: usize,
+        population: Vec<usize>,
+    ) -> Annealer<F, G> {
+        let state = vec![0_usize; precinct_in.len()];
+        let mut annealer = Annealer {
+            objective,
+            temperature,
+            adj,
+            precinct_in,
+            num_districts,
+            cur_state: (0.0, state),
+            best: (0.0, Vec::new()),
+            population,
+            border_nodes: BTreeMap::new(),
+        };
+        annealer.cur_state.0 =
+            (annealer.objective)(&annealer.state_as_input::<fn(usize) -> usize>(None));
+        annealer.best = annealer.cur_state.clone();
+        annealer.border_nodes = annealer.get_border_nodes();
+        dbg!(&annealer);
+        annealer
+    }
+
+    fn get_border_nodes(&self) -> BTreeMap<usize, usize> {
+        BTreeMap::from_iter((0..self.precinct_in.iter().len()).map(|x| {
+            (
+                x,
+                self.adj[x].iter().fold(0, |acc, elem| {
+                    acc + (self.cur_state.1[*elem] != self.cur_state.1[x]) as usize
+                }),
+            )
+        }))
+    }
+
+    // assignment_indexer is used for score_change, to make it &self instead of &mut self
+    fn state_as_input<H: Fn(usize) -> usize>(
+        &self,
+        assignment_indexer: Option<H>,
+    ) -> Vec<Vec<bool>> {
+        let assignment_indexer: &dyn Fn(usize) -> usize = match &assignment_indexer {
+            Some(x) => x,
+            None => &|x| self.cur_state.1[x],
+        };
+        let mut result = vec![vec![false; self.precinct_in.len()]; self.num_districts];
+        for precinct in self.precinct_in.iter() {
+            result[assignment_indexer(*precinct)][*precinct] = true;
+        }
+        result
+    }
+
+    pub fn anneal(&mut self, num_steps: usize) -> PyResult<(f64, Vec<usize>)> {
+        let mut rand_state = UniformDist::new([0xfda52833df686ae6, 0x7919f78c90a9362c]);
+        for step in 0..num_steps {
+            let temp = (self.temperature)(step as f64 / num_steps as f64);
+            let scores: Vec<(usize, usize, f64)> = self
+                .border_nodes
+                .iter()
+                .flat_map(|(node, _)| {
+                    self.adj[*node]
+                        .iter()
+                        .filter_map(|neighbor| {
+                            if self.cur_state.1[*neighbor] == self.cur_state.1[*node] {
+                                None
+                            } else {
+                                Some(self.cur_state.1[*neighbor])
+                            }
+                        })
+                        .unique()
+                        .map(|district_num| {
+                            (
+                                *node,
+                                district_num,
+                                (self.score_change(*node, district_num) / temp).exp(),
+                            )
+                        })
+                })
+                .collect();
+            let sum: f64 = scores.iter().fold(0.0, |acc, (_, _, elem)| acc + elem);
+            let next_probabilities = scores.iter().map(|(_, _, x)| x / sum);
+            let rand = rand_state.next() as f64 / MAX;
+            let mut accumulated_probability = 0.0;
+            for (i, probability) in next_probabilities.enumerate() {
+                accumulated_probability += probability;
+                if accumulated_probability > rand {
+                    self.cur_state.1[scores[i].0] = scores[i].1;
+                    self.cur_state.0 =
+                        (self.objective)(&self.state_as_input::<fn(usize) -> usize>(None));
+                    if self.cur_state.0 > self.best.0 {
+                        self.best = self.cur_state.clone();
+                    }
+                    break;
+                }
+            }
+        }
+        Ok(self.cur_state.clone())
+    }
+
+    fn score_change(&self, node: usize, district_num: usize) -> f64 {
+        let score = (self.objective)(&self.state_as_input(Some(|i| {
+            if i == node {
+                district_num
+            } else {
+                self.cur_state.1[i]
+            }
+        })));
+        score
+    }
+
+    fn update_borders(&mut self, node: usize, new_state: usize) {
+        for neighbor in self.adj[node].iter() {
+            if new_state == self.cur_state.1[*neighbor]
+                && self.cur_state.1[node] != self.cur_state.1[*neighbor]
+            {
+                *self.border_nodes.get_mut(neighbor).unwrap() -= 1;
+            } else if self.cur_state.1[node] == self.cur_state.1[*neighbor] {
+                match self.border_nodes.get_mut(neighbor) {
+                    Some(v) => *v += 1,
+                    None => _ = self.border_nodes.insert(*neighbor, 1),
+                }
+            }
+        }
+    }
+
+    fn feasible(&self) -> bool {
+        let populations = self.population.iter().enumerate().fold(
+            vec![0_usize; self.num_districts],
+            |mut acc, (i, elem)| {
+                acc[self.precinct_in[i]] += elem;
+                acc
+            },
+        );
+        let mean = populations.iter().sum::<usize>() as f64 / populations.len() as f64;
+        for p in populations {
+            if !((1.0 - MAX_POP_DIFF) * mean..(1.0 + MAX_POP_DIFF) * mean).contains(&(p as f64)) {
+                return false;
+            }
+        }
+        let mut flood_filled = vec![false; self.num_districts];
+        let mut vis = vec![self.num_districts; self.adj.len()];
+        for node in 0..self.precinct_in.len() {
+            if vis[node] != self.num_districts {
+                continue;
+            }
+            if !flood_filled[self.cur_state.1[node]] {
+                self.feasible_helper(node, &mut vis);
+                flood_filled[self.cur_state.1[node]] = true;
+            } else if vis[node] == self.num_districts {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn feasible_helper(&self, node: usize, vis: &mut Vec<usize>) {
+        vis[node] = self.cur_state.1[node];
+        for next in self.adj[node]
+            .iter()
+            .filter(|n| vis[**n] == vis[node])
+            .collect::<Vec<&usize>>()
+        {
+            self.feasible_helper(*next, vis);
+        }
+    }
+}
---
+++ sim-anneal/src/lib.rs
@@ -0,0 +1,69 @@
+use pyo3::{
+    exceptions::PyException,
+    pyfunction, pymodule,
+    types::{PyAnyMethods, PyModule},
+    wrap_pyfunction, Bound, PyAny, PyErr, PyResult,
+};
+mod anneal;
+
+///@param objective callable objective function
+///@param precinct_in: a vector of all precincts, and which pixel they are in; pixels are row *
+///width + col
+///@param adj a 2d vector adjacency list for the precincts
+///@param num_districts the number of districts to be planned
+///@param population the population of each precinct (indices should match precinct_in, adj)
+#[pyfunction]
+fn optimize_func(
+    objective_raw: Bound<'_, PyAny>,
+    temperature_raw: Bound<'_, PyAny>,
+    precinct_in: Vec<usize>,
+    adj: Vec<Vec<usize>>,
+    num_districts: usize,
+    population: Vec<usize>,
+    num_steps: usize,
+) -> PyResult<(f64, Vec<usize>)> {
+    if adj.len() != precinct_in.len() {
+        return PyResult::Err(PyErr::new::<PyException, _>(PyException::new_err(
+            "adj.len() != precinct_in.len",
+        )));
+    }
+    if !objective_raw.is_callable() {
+        return PyResult::Err(PyErr::new::<PyException, _>(PyException::new_err(
+            "objective argument must be callable",
+        )));
+    }
+    let objective = |v: &[Vec<bool>]| {
+        objective_raw
+            .call1((v.to_vec(),))
+            .unwrap()
+            .extract()
+            .expect("objective returned wrong type, must retur f64")
+    };
+    if !temperature_raw.is_callable() {
+        return PyResult::Err(PyErr::new::<PyException, _>(PyException::new_err(
+            "temperature argument must be callable",
+        )));
+    }
+    let temperature = |x: f64| {
+        objective_raw
+            .call1((x,))
+            .unwrap()
+            .extract()
+            .expect("temperature returned wrong type, must retur f64")
+    };
+    let mut annealer = anneal::Annealer::new(
+        precinct_in,
+        adj,
+        objective,
+        temperature,
+        num_districts,
+        population,
+    );
+    annealer.anneal(num_steps)
+}
+
+#[pymodule]
+fn sim_anneal(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(optimize_func, m)?)?;
+    Ok(())
+}
